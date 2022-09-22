735ee6262569b428da84f95c337d774c9d9df161
김동희 <dongh94@naver.com>
2022-09-22 23:25:10 +0900

    [feat]Front: solidity test

--- frontend/src/pages/customer/MyPage.js
+++ frontend/src/pages/customer/MyPage.js
@@ -1,10 +1,54 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
 
-const MyPage = () => {
-    return (
-        <div>
-            <p>My Page</p>
-        </div>
-    );
-};
+function Mypage() {
+  const [account, setAccount] = useState();
+  const [contactList, setContactList] = useState();
+  const [contacts, setContacts] = useState([]);
 
-export default MyPage;
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/7885ac55f47f453488027010d12acadb"));
+
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      // Instantiate smart contract using ABI and address.
+      const contactList = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+      // set contact list to state variable.
+      setContactList(contactList);
+      // Then we get total number of contacts for iteration
+      const counter = await contactList.methods.count().call();
+      // iterate through the amount of time of counter
+      for (var i = 1; i <= counter; i++) {
+        // call the contacts method to get that particular contact from smart contract
+        const contact = await contactList.methods.contacts(i).call();
+        // add recently fetched contact to state variable.
+        setContacts((contacts) => [...contacts, contact]);
+      }
+    }
+
+    load();
+  }, []);
+
+  return (
+    <div>
+      Your account is: {account}
+      <h1>Contacts</h1>
+      <ul>
+        {Object.keys(contacts).map((contact, index) => (
+          <li key={`${contacts[index].name}-${index}`}>
+            <h4>{contacts[index].name}</h4>
+            <span>
+              <b>Phone: </b>
+              {contacts[index].phone}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Mypage;
