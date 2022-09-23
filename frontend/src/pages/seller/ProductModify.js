315ff67d3333c691b72ecff1557411f11d10f960
jinha <jinhasky@naver.com>
2022-09-23 13:45:10 +0900

    [feat]:Front:productform

--- frontend/src/pages/seller/ProductModify.js
+++ frontend/src/pages/seller/ProductModify.js
@@ -1,9 +1,13 @@
-
+import ProductForm from '../../components/ProductForm';
+import MKBtn from '../../components/ui/MKBtn';
 const ProductModify = () => {
     return (
-        <div>
-            <p>Product Modify</p>
-        </div>
+        <div className='container'>
+        <h1>상품 수정</h1>
+        <hr />
+        <ProductForm/> 
+        <MKBtn >수정하기</MKBtn>
+    </div>
     );
 };
 
