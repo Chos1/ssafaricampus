8ea2de427121b2c4856dbe2836590af94d3c1d9c
조성환 <chosw0812@naver.com>
2022-09-25 16:45:43 +0900

    Revert "Merge branch 'fe/product_css' into 'main'"
    This reverts merge request !39

--- frontend/src/components/ui/XsPBtn.js
+++
@@ -1,9 +0,0 @@
-import styles from './XsPBtn.module.css';
-const XsPBtn = (props) => {
-  const classes = styles.XsPBtn + ' ' + props.className;
-  return (
-    <button className={classes} onClick={props.onClick}>{props.children}</button>
-  );
-};
-
-export default XsPBtn;
