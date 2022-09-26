2ce8bb6803b05e1828d8c8efceb575b095dc6286
kahyu <rkd9755@naver.com>
2022-09-26 16:49:04 +0900

    [fix] Front: console error 수정, selloer form 요청 url 수정, redux persist 설치

--- frontend/src/components/PurchaseRequest/RequestForm.js
+++ frontend/src/components/PurchaseRequest/RequestForm.js
@@ -11,20 +11,20 @@ const RequestForm = () => {
           
           <form className="request_inputgroup">
 
-          <label for="">배송 주소</label>
+          <label>배송 주소</label>
           <input placeholder="배송 주소 입력해주세요" className='shipping_add'/>
           <SWBtnPBrd className="add_search">검색</SWBtnPBrd>
           <br/>
-          <label for="">지갑 주소</label>
+          <label>지갑 주소</label>
           <input placeholder="지갑 주소를 입력해주세요" />
           <br/>
-          <label for="">총 인원</label>
+          <label>총 인원</label>
           <input placeholder="총 인원을 입력해주세요"/>
           <br/>
-          <label for="">첨부 파일</label>
+          <label>첨부 파일</label>
           <input type="file" placeholder="총 인원을 입력해주세요" className='add_file'/>
           <br/>
-          <label for="">비밀번호</label>
+          <label>비밀번호</label>
           <input placeholder="비밀번호를 입력해주세요"/>
           <br/> 
           <MPBtn onClick={() => {navigate('/purchaseContract/:transactionId')}}>신청하기</MPBtn>
--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialUserState = {
-  role: 'role',
-  name: 'name',
-  email: 'email',
-  phone: 'phone'
+  role: '',
+  name: '',
+  email: '',
+  phone: ''
 };
 
 const userSlice = createSlice({
