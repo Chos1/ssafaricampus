fe5e9ffeb668f113a3cfd1bb0b8ac67887e266a0
조성환 <chosw0812@naver.com>
2022-09-28 13:11:56 +0900

    [fix]Front : Wallet 수정

--- frontend/src/components/UserInfo/WalletItem.js
+++ frontend/src/components/UserInfo/WalletItem.js
@@ -1,5 +1,6 @@
 import WalletCarousel from "./WalletCarousel";
 import AddWalletBtn from "./AddWalletBtn";
+import ReactTooltip from "react-tooltip";
 
 import styles from "./WalletItem.module.css";
 
@@ -8,13 +9,24 @@ import styles from "./WalletItem.module.css";
 const WalletItem = (props) => {
 
   const Wallet = props.isWallet ? <WalletCarousel /> : <AddWalletBtn />;
-
+  const doCopy = text => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert("클립보드에 복사되었습니다.");
+      })
+      .catch(() => {
+        alert("복사를 다시 시도해주세요.");
+      });
+  };
   return (
     <div className={styles.WalletItem}>
       <div className={styles.WalletTitle}>
         <h2>지갑관리</h2>
-        <h3> ? </h3>
-        <p className={styles.arrow_box}>여기에 지갑 주소?</p>
+        <h3 data-for="walletName" data-tip onClick={() => doCopy("지갑주소12341234")}> ? </h3>
+        <ReactTooltip
+        id="walletName"
+        getContent={dataTip => "지갑주소"}></ReactTooltip>
       </div>
       {/* <WalletCarousel />
       <AddWalletBtn /> */}
