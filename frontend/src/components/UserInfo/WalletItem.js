857c241babf58f75e72dd0cec592514a9da275a7
조성환 <chosw0812@naver.com>
2022-09-28 16:22:29 +0900

    [fix]Front : icon추가

--- frontend/src/components/UserInfo/WalletItem.js
+++ frontend/src/components/UserInfo/WalletItem.js
@@ -2,12 +2,14 @@ import WalletCarousel from "./WalletCarousel";
 import AddWalletBtn from "./AddWalletBtn";
 import ReactTooltip from "react-tooltip";
 
+import {AiOutlineInfoCircle} from "react-icons/ai"
+
 import styles from "./WalletItem.module.css";
 
 
 
 const WalletItem = (props) => {
-
+  
   const Wallet = props.isWallet ? <WalletCarousel /> : <AddWalletBtn />;
   const doCopy = text => {
     navigator.clipboard
@@ -23,13 +25,11 @@ const WalletItem = (props) => {
     <div className={styles.WalletItem}>
       <div className={styles.WalletTitle}>
         <h2>지갑관리</h2>
-        <h3 data-for="walletName" data-tip onClick={() => doCopy("지갑주소12341234")}> ? </h3>
+        <AiOutlineInfoCircle data-for="walletName" data-tip onClick={() => doCopy("지갑주소12341234")} />
         <ReactTooltip
         id="walletName"
         getContent={dataTip => "지갑주소"}></ReactTooltip>
       </div>
-      {/* <WalletCarousel />
-      <AddWalletBtn /> */}
       {Wallet}
     </div>
   );
