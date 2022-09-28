ce45a454d7e87e6f3cf63cc4664d61ed3a1e414e
조성환 <chosw0812@naver.com>
2022-09-28 13:49:04 +0900

    [fix]Front : css 수정

--- frontend/src/pages/customer/MyPage.js
+++ frontend/src/pages/customer/MyPage.js
@@ -5,16 +5,13 @@ import PurchaseList from '../../components/UserInfo/PurchaseList';
 import styles from './Mypage.module.css'
 
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import Web3 from "web3";
+import { useState } from "react";
+// import Web3 from "web3";
 // import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
 import { useSelector } from 'react-redux';
 
 function Mypage() {
-  const [account, setAccount] = useState();
-  const [contactList, setContactList] = useState();
-  const [contacts, setContacts] = useState([]);
-  
+ 
   const name = useSelector((state) => state.user.name);
   const loginId = useSelector((state) => state.user.loginId);
   const email = useSelector((state) => state.user.email);
@@ -24,31 +21,6 @@ function Mypage() {
 
   const [isWallet, setIsWallet] = useState(true);
   const item = isWallet ? <PurchaseList /> : <Notion />;
-
-  useEffect(() => {
-    async function load() {
-      const web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/7885ac55f47f453488027010d12acadb"));
-
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
-      // Instantiate smart contract using ABI and address.
-      // const contactList = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
-      // set contact list to state variable.
-      setContactList(contactList);
-      // Then we get total number of contacts for iteration
-      const counter = await contactList.methods.count().call();
-      // iterate through the amount of time of counter
-      for (var i = 1; i <= counter; i++) {
-        // call the contacts method to get that particular contact from smart contract
-        const contact = await contactList.methods.contacts(i).call();
-        // add recently fetched contact to state variable.
-        setContacts((contacts) => [...contacts, contact]);
-      }
-    }
-
-    load();
-  }, []);
-
   
   return (
     <div className={styles.mypage}>
