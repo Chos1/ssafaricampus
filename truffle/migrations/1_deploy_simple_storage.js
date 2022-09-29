f1501e2d07aa7baa2e05eb921ea9aa8b63e66dde
sonic1 <sonic1@naver.com>
2022-09-29 11:47:21 +0900

    [fix] Blockchain: truffle deploy test to frontend contracts

--- truffle/migrations/1_deploy_simple_storage.js
+++ truffle/migrations/1_deploy_simple_storage.js
@@ -1,9 +1,11 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
+const Test = artifacts.require("Test");
 const Cash = artifacts.require("Cash");
 const EscrowFactory = artifacts.require("EscrowFactory");
 const PurchaseRecord = artifacts.require("PurchaseRecord");
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Test);
   // deployer.deploy(Cash);
   // deployer.deploy(PurchaseRecord);
   // deployer.deploy(EscrowFactory);
