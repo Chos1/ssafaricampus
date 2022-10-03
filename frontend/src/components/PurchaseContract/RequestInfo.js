12aab3af3b1c53d07f90cf18149fdb359138b484
sonic1 <sonic1@naver.com>
2022-10-03 10:52:56 +0900

    [feat] Front: 구매계약 페이지 연동

--- frontend/src/components/PurchaseContract/RequestInfo.js
+++ frontend/src/components/PurchaseContract/RequestInfo.js
@@ -14,36 +14,32 @@ const RequestInfo = () => {
 
   useEffect(() => {
     const getContractDetails = async () => {
-      const contractDetail = await contract.methods
-        .viewPurchaseContract()
-        .call({ from: account });
+      const contractDetail = await contract.methods.viewPurchaseContractByPurchaseNod(contract_No).call({ from: account });
       setContractDetail(contractDetail);
     };
 
     getContractDetails();
-    console.log("contractDetail: " + contractDetail);
   }, [account, contract, contract_No]);
+  console.log(contractDetail);
 
   return (
     <div className="request_info">
       <div>
         <div className="request_item">
           <p className="request_label">배송지</p>
-          <p className="request_content">
-            대전 광역시 어딘가 어느 건물 근처 거기
-          </p>
+          <p className="request_content">{contractDetail.shipping_address}</p>
         </div>
         <div className="request_item">
           <p className="request_label">지갑 주소</p>
-          <p className="request_content">0x2C9908F99f1a5195195dk2i3o2o1</p>
+          <p className="request_content">{contractDetail.purchase_address}</p>
         </div>
         <div className="request_item">
           <p className="request_label">총 인원</p>
-          <p className="request_content">120 명</p>
+          <p className="request_content">{contractDetail.total_people} 명</p>
         </div>
         <div className="request_item">
           <p className="request_label">총 가격</p>
-          <p className="request_content">1,548,000 원</p>
+          <p className="request_content">{(contractDetail.total_price * 1).toLocaleString("ko-KR")} 원</p>
         </div>
       </div>
     </div>
