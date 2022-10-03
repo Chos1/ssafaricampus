befa4cbdb54c23fe47fd196c4c540dae116ece53
김동희 <dongh94@naver.com>
2022-10-03 20:43:58 +0900

    [feat] Front: 솔리디티 결제하기 기능 구현

--- frontend/src/components/PurchaseContract/ModalBasic.js
+++ frontend/src/components/PurchaseContract/ModalBasic.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import './ModalBasic.module.css';
+import React from "react";
+import "./ModalBasic.module.css";
+import useEth from "../../contexts/EthContext/useEth";
+import Web3 from "web3";
 
 import { useState } from "react";
 
 const ModalBasic = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  const { open, close, header, cont_pass } = props;
+  const { open, close, header, cont_pass, itemNo, contract_No } = props;
+  const {
+    state: { contract, account },
+  } = useEth();
 
   const [password, setPassword] = useState("");
 
   const passwordChangeHandler = (e) => {
     setPassword(e.target.value);
   };
-
-  const passCheck = (e) => {
-    if (cont_pass === password){
-      alert("asdasdasd")
-      close()
-    }else{
-      alert("응 안돼")
+  const payContract = async () => {
+    // item_No, uint _purchase_No, address _paid_address
+    const itemDetail = await contract.methods.viewItemByItemNo(itemNo).call({ from: account });
+    const etherprice = itemDetail.item_price / 20000000;
+    const web3 = new Web3(Web3.givenProvider || "https://goerli.infura.io/v3/7885ac55f47f453488027010d12acadb");
+    await contract.methods.paidContract(itemNo, contract_No, account).send({ from: account, gas: 412040, value: web3.utils.toWei(`${etherprice}`, "ether") });
+  };
+  const passCheck = async (e) => {
+    if (cont_pass === password) {
+      await payContract();
+      alert("결제가 완료되었습니다.");
+      close();
+    } else {
+      alert("응 안돼");
     }
-  }
-  
+  };
+
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className={open ? 'openModal modal' : 'modal'}>
+    <div className={open ? "openModal modal" : "modal"}>
       {open ? (
         <section>
           <header>
@@ -33,9 +45,10 @@ const ModalBasic = (props) => {
               &times;
             </button>
           </header>
-          <main>{props.children}
-          <label for="contract_pass">비밀번호</label>
-          <input type="password" value={password} onChange={passwordChangeHandler}/>
+          <main>
+            {props.children}
+            <label>비밀번호</label>
+            <input type="password" value={password} onChange={passwordChangeHandler} />
           </main>
           <footer>
             <button className="close" onClick={close}>
