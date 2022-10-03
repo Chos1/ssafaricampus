d660b98efa6474b2192888ee15a53150c52cecda
KangHyun You <kanghyunyou@KangHyunui-MacBookAir.local>
2022-10-04 00:23:12 +0900

    [fix] Front: 검색기능 사용시 배너 없이 보여주기

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 import Banner from "../components/MainPage/Banner.js";
 import ProductCarousel from "../components/MainPage/ProductCarousel.js";
 const MainPage = () => {
+  const banner = useLocation().pathname.split("/")[2] ? null : <Banner />;
+  console.log(banner);
+
   return (
     <div>
-      <Banner />
+      {banner}
       <ProductCarousel />
     </div>
   );
