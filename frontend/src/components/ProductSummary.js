baec8cec08d11c92336e6d4c403f23c6898a0ad7
KangHyun You <kanghyunyou@KangHyunui-MacBookAir.local>
2022-10-03 13:38:06 +0900

    [feat] Front: 구매신청관련 데이터들 바인딩 완료

--- frontend/src/components/ProductSummary.js
+++ frontend/src/components/ProductSummary.js
@@ -1,19 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 import "./ProductSummary.css";
 import LPBtn from "./ui/LPBtn";
 // import groom from '../assets/구름.jpg';
 import MKBtn from "./ui/MKBtn";
+import useEth from "../contexts/EthContext/useEth";
 
 const ProductSummary = (props) => {
   const navigate = useNavigate();
-  const name = useSelector((state) => state.user.name);
+  // const name = useSelector((state) => state.user.name);
   const myRole = useSelector((state) => state.user.role);
 
   const nowPath = window.location.pathname.split("/")[1];
-  const item_No = window.location.pathname.split("/")[2];
+  const item_No =
+    nowPath === "purchaseContract"
+      ? props.itemNo
+      : window.location.pathname.split("/")[2];
 
+  // purchaseContract 페이지일 경우 info data 결정
+  const {
+    state: { contract, account },
+  } = useEth();
+  const [itemDetail, setItemDetail] = useState("");
+
+  useEffect(() => {
+    if (nowPath === "purchaseContract") {
+      const getContractDetails = async () => {
+        const contractDetail = await contract.methods
+          .viewItemByItemNo(item_No)
+          .call({ from: account });
+        setItemDetail(contractDetail);
+      };
+
+      getContractDetails();
+    }
+  }, [account, contract, item_No, nowPath]);
+  console.log("item detail: " + itemDetail);
+
+  // 버튼 형태 결정
   let changeComponent = "";
   const price_component =
     myRole === "COMPANY" ? "product_price_seller" : "product_price";
@@ -39,66 +65,52 @@ const ProductSummary = (props) => {
     );
   }
 
-  // switch(nowPath) {
-  //   case 'products':
-  //     changeComponent = <LPBtn
-  //       onClick={() => {
-  //         navigate('/productRequest/:productId')
-  //       }}
-  //       className='purchase_btn1'
-  //     >
-  //       구매하기
-  //     </LPBtn>
-  //     break;
-
-  //   case 'purchaseContract':
-  //     changeComponent = <div></div>
-  //     break;
-
-  //   case 'productManage':
-  //     changeComponent =
-  //     <div>
-  //       <MKBtn>등록하기</MKBtn>
-  //       <MKBtn>삭제하기</MKBtn>
-  //     </div>
-  //     break;
-
-  //   default:
-  //     break;
-  // }
-
   return (
     <div className="product_summary_center">
       <div className="product_summary">
         <div>
-          <img src={props.tUrl} alt="" />
+          <img src={props.tUrl || itemDetail.thumbnail_picture} alt="" />
         </div>
         <div className="product_summary_de">
           <div className="product_main">
-            <p className="product_title">{props.title}</p>
-            <p className="product_subtitle">{props.subtitle}</p>
-            <p className={price_component}>{props.price}원</p>
+            <p className="product_title">
+              {props.title || itemDetail.item_name}
+            </p>
+            <p className="product_subtitle">
+              {props.subtitle || itemDetail.oneline_description}
+            </p>
+            <p className={price_component}>
+              {props.price || itemDetail.item_price}원
+            </p>
           </div>
           <div>
             <div className="product_mini">
               <p className="product_label">배송</p>
-              <p className="product_content">{props.period}</p>
+              <p className="product_content">
+                {props.period || itemDetail.delivery_period}
+              </p>
             </div>
             <div className="product_mini">
               <p className="product_label">판매자</p>
-              <p className="product_content">{name}</p>
+              <p className="product_content">판매자 이름 넣어야해</p>
             </div>
             <div className="product_mini">
               <p className="product_label">판매단위</p>
-              <p className="product_content">{props.sales_unit}개</p>
+              <p className="product_content">
+                {props.sales_unit || itemDetail.sales_unit}개
+              </p>
             </div>
             <div className="product_mini">
               <p className="product_label">원산지</p>
-              <p className="product_content">{props.origin}</p>
+              <p className="product_content">
+                {props.origin || itemDetail.item_origin}
+              </p>
             </div>
             <div className="product_mini_last">
               <p className="product_label">상품설명</p>
-              <p className="product_content">{props.descript}</p>
+              <p className="product_content">
+                {props.descript || itemDetail.detail_description}
+              </p>
             </div>
           </div>
           <br />
