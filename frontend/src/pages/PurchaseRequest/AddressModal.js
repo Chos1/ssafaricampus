2b19fd877ce870bbf472bb25b201bba6503b0919
조성환 <chosw0812@naver.com>
2022-10-05 12:38:38 +0900

    [fix] Front: modal css 분리

--- frontend/src/pages/PurchaseRequest/AddressModal.js
+++ frontend/src/pages/PurchaseRequest/AddressModal.js
@@ -9,7 +9,7 @@ const Modal = (props) => {
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className={open ? "openModal modal" : "modal"}>
+    <div className={open ? "openModal modal1" : "modal1"}>
       {open ? (
         <section>
           <header>
