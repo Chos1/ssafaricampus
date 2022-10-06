a392c8b67e28774ecba82613ca1ef92b2676b91e
KangHyun You <kanghyunyou@KangHyunui-MacBookAir.local>
2022-10-06 19:58:40 +0900

    [feat] Front: 대학생 패키지 배너 상품 페이지 연결

--- frontend/src/pages/MainPage/ProductCarousel.js
+++ frontend/src/pages/MainPage/ProductCarousel.js
@@ -36,7 +36,9 @@ const ProductCarousel = () => {
     const FoodDummy = [];
     const Setting_itemsDummy = [];
     const displayItems = async () => {
-      const itemArray = await contract.methods.viewItems().call({ from: account });
+      const itemArray = await contract.methods
+        .viewItems()
+        .call({ from: account });
       for (let i = itemArray.length - 1; i >= 0; i--) {
         if (parseInt(itemArray[i][0]) >= 0) {
           if (location.split("/")[2] && !itemArray[i][1].includes(keyword)) {
@@ -85,7 +87,13 @@ const ProductCarousel = () => {
     Clothes.length < 1 ? null : (
       <div className="Product_div">
         <h2 className="ProductList_title">의류</h2>
-        <Swiper grabCursor={true} className="mySwiper" slidesPerView={4} navigation={true} modules={[Navigation]}>
+        <Swiper
+          grabCursor={true}
+          className="mySwiper"
+          slidesPerView={4}
+          navigation={true}
+          modules={[Navigation]}
+        >
           {ClothesCarousel}
         </Swiper>
       </div>
@@ -112,7 +120,13 @@ const ProductCarousel = () => {
     Foods.length < 1 ? null : (
       <div className="Product_div">
         <h2 className="ProductList_title">식품</h2>
-        <Swiper grabCursor={true} className="mySwiper" slidesPerView={4} navigation={true} modules={[Navigation]}>
+        <Swiper
+          grabCursor={true}
+          className="mySwiper"
+          slidesPerView={4}
+          navigation={true}
+          modules={[Navigation]}
+        >
           {FoodCarousel}
         </Swiper>
       </div>
@@ -128,6 +142,7 @@ const ProductCarousel = () => {
         },
       });
     };
+
     return (
       <SwiperSlide key={idx} onClick={move} className="border_out">
         <ProductItem item={item} />
@@ -139,16 +154,30 @@ const ProductCarousel = () => {
     setting_items.length < 1 ? null : (
       <div className="Product_div">
         <h2 className="ProductList_title">잡화</h2>
-        <Swiper grabCursor={true} className="mySwiper" slidesPerView={4} navigation={true} modules={[Navigation]}>
+        <Swiper
+          grabCursor={true}
+          className="mySwiper"
+          slidesPerView={4}
+          navigation={true}
+          modules={[Navigation]}
+        >
           {SettingCarousel}
         </Swiper>
       </div>
     );
 
+  const moveEventItem = () => {
+    navigate("/products/8");
+  };
+
   return (
     <div className="App">
       {FoodList}
-      <div className="Product_div">{isSearch ? null : <img className="Event_Banner" src={BannerImg} alt="" />}</div>
+      <div className="Product_div" onClick={moveEventItem}>
+        {isSearch ? null : (
+          <img className="Event_Banner" src={BannerImg} alt="" />
+        )}
+      </div>
       {ClothList}
       {OtherList}
     </div>
