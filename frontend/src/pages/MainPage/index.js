9267d070e54386836a595eb0fb0d89c7a8e830ea
KangHyun You <kanghyunyou@KangHyunui-MacBookAir.local>
2022-10-06 16:49:50 +0900

    [fix] Front: 배너 다시 보이게 수정

--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -1,32 +1,26 @@
 // packages
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-// import cogoToast from 'cogo-toast';
+// utils
+import useEth from "../../contexts/EthContext/useEth";
 // components
 import Banner from "./Banner.js";
 import ProductCarousel from "./ProductCarousel.js";
 import Loading from "../../components/ui/Loading.js";
-
-import useEth from "../../contexts/EthContext/useEth";
-
-import styles from "./css/MainHeader.module.css"
+// css
+import styles from "./css/MainHeader.module.css";
 
 const MainPage = () => {
-  const location = useLocation();
   const { state } = useEth();
   const navigate = useNavigate();
   const [TimeCheck, setTimeCheck] = useState(true);
 
   let MainCompo = <></>;
-  const isSearch = location.pathname.split('/')[3] ? true : false;
-  const banner = isSearch ? null : <Banner />;
-
- console.log('젠킨스 가동!!!!');
 
   if (state.account) {
     MainCompo = (
       <>
-        {banner}
+        <Banner />
         <ProductCarousel />
       </>
     );
@@ -35,14 +29,15 @@ const MainPage = () => {
       setTimeout(() => {
         setTimeCheck(false);
       }, 1000);
-      MainCompo = 
-      <div className={styles.loading_center}>
-        <Loading />
-      </div>
+      MainCompo = (
+        <div className={styles.loading_center}>
+          <Loading />
+        </div>
+      );
     } else {
-      alert("MyPage에서 지갑을 연결하세요")
+      alert("MyPage에서 지갑을 연결하세요");
       // cogoToast.success('지갑을 연결하세요');
-      navigate('/mypage')
+      navigate("/mypage");
     }
   }
 
