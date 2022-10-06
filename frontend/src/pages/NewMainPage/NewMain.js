2fc940ed3ba71c7deca64ea038c5f3b7be71ff18
jinha <jinhasky@naver.com>
2022-10-07 03:26:02 +0900

    [fix] Front : 메인화면 바꾸기

--- frontend/src/pages/NewMainPage/NewMain.js
+++ frontend/src/pages/NewMainPage/NewMain.js
@@ -5,6 +5,8 @@ import "./NewMain.css";
 import LPBtn from "../../components/ui/LPBtn";
 import LWBtnPBrd from "../../components/ui/LWBtnPBrd";
 // assets
+import homesaza from "../../assets/홈사장.svg";
+import homepanda from "../../assets/홈판당.svg";
 import { ReactComponent as Logo } from "../../assets/Logo.svg";
 // import banner from "../../assets/배너.PNG";
 // import groom from "../../assets/구름.jpg";
@@ -19,26 +21,32 @@ const NewMain = () => {
         <img src={groom} alt="" className="img1" />
       </div> */}
       <div className="whole_box">
-        <div className="logo_center">
-          <Logo width="900px" className="center_img" />
-        </div>
-        {/* <img src={image1} alt="" className="img1" />
+        <div>
+          <div className="logo_center">
+            <Logo width="900px" className="center_img" />
+          </div>
+          <div className="last">
+            <img src={homesaza} alt="" className="saza" />
+            <div className="main_new ">
+              <LPBtn
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                로그인
+              </LPBtn>
+              <LWBtnPBrd
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                회원가입
+              </LWBtnPBrd>
+            </div>
+            <img src={homepanda} alt="" className="panda" />
+          </div>
+          {/* <img src={image1} alt="" className="img1" />
         <img src={image2} alt="" className="img2" /> */}
-        <div className="main_new ">
-          <LPBtn
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            로그인
-          </LPBtn>
-          <LWBtnPBrd
-            onClick={() => {
-              navigate("/signup");
-            }}
-          >
-            회원가입
-          </LWBtnPBrd>
         </div>
       </div>
     </div>
